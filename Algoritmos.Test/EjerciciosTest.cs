b4bd5c5e6b8fabee0257137840562ca05b109d64
Mariano Antonio Montano <montano.mariano@gmail.com>
2021-01-02 19:34:14 -0300

    Agregar archivos de proyecto.

---
+++ Algoritmos.Test/EjerciciosTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Algoritmos.Ejercicios;
+
+namespace Algoritmos.Test
+{
+    public class EjerciciosTest
+    {
+
+        [Theory]
+        [InlineData("hola", "ola", "ola")]
+        [InlineData("para", "roca", "ar")]
+        [InlineData("masa", "poco", "")]
+        public void LetrasDuplicadas_DevuelveListaDeDuplicados
+            (string input1, string input2, string output)
+        {
+            //Act
+            var actual = EjerciciosVarios.LetrasDuplicadas(input1, input2);
+
+            //Assert
+            Assert.Equal(output, actual);
+        }
+
+        [Theory]
+        [InlineData("Mi nombre es Mariano Montano", 5)]
+        [InlineData("El perro de mi vecino se llama Tommy", 8)]
+        [InlineData("Me llamo Mariano y tengo dos hijas muy hermosas que se la pasan haciendo lío", 15)]
+        public void ContarPalabrasEnCadena_RetornaCantidadDePalabras(string input, int resultado)
+        {
+            //Act
+            var actual = EjerciciosVarios.ContarPalabrasEnCadena(input);
+
+            //Assert
+            Assert.Equal(resultado, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 9)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 5)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 7)]
+        public void SumarDosNumerosDeArray_RetornaVerdadero(int[] array, int resultado)
+        {
+            //Act
+            var actual = EjerciciosVarios.SumarDosNumerosDeArray(array, resultado);
+
+            //Assert
+            Assert.True(actual);
+        }
+
+    }
+}
---
+++ Algoritmos/Ejercicios/EjerciciosVarios.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos.Ejercicios
+{
+    public static class EjerciciosVarios
+    {
+        //How to Print duplicate characters in a String
+        public static string LetrasDuplicadas(string input1, string input2)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var array1 = input1.ToCharArray();
+            var array2 = input2.ToCharArray();
+
+            //tomo letra de la primera palabra
+            for (int i = 0; i < array1.Length; i++)
+            {
+                //comparo con letra de la segunda palabra
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    //si son iguales y no está en la lista de resultado
+                    if (array1[i] == array2[j] && output.ToString().Contains(array1[i].ToString()) == false)
+                    {
+                        //agrego a la lista de resultado
+                        output.Append(array1[i].ToString());
+                    }
+                }
+            }
+
+            return output.ToString();
+        }
+
+       //How to count number of words in a String
+        public static int ContarPalabrasEnCadena(string input)
+        {
+            int resultado = 0;
+
+            var separada = input.Split(' ');
+
+            resultado = separada.Count();
+
+            return resultado;
+        }
+
+        //Determine if any two integers in array sum to given integer
+        public static bool SumarDosNumerosDeArray(int[] array, int suma)
+        {
+            bool resultado = false;
+            //desde el primero hasta el anteúltimo
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                //desde el segundo hasta el último
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == suma)
+                    {
+                        resultado = true;
+                    }
+                }
+            }
+
+            return resultado;
+        }
+
+
+    }
+}
